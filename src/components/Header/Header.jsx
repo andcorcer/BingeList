85bea425298b4320b786c74133cc3160db3cf30f
Andrés Córdova <andcorcer2006@gmail.com>
2026-08-27 15:36:34 -0600

    Added styles for the Header and Navbar components and updated the global styles for 'btn' and 'btn-primary' classes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
   return (
     <header className="header">
       <div className="title-logo">
-        <h1>
+        <h1 className="title">
           <Link to="/">BingeList</Link>
         </h1>
       </div>
